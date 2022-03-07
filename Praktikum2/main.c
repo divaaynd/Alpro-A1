4cd3541ccabcdcb69adccd5613980d87b18a9d62
divaaynd <100470946+divaaynd@users.noreply.github.com>
2022-03-07 21:03:14 +0900

    Add files via upload

---
+++ Praktikum2/main.c
@@ -0,0 +1,22 @@
+/*Nama File	: BujurSangkar.c*/
+/*Deskripsi	: Menghitung bujur sangkar*/
+/*Pembuat	: Diva Ayunda Najwina - 24060121140101*/
+/*Tgl pembuatan	: Senin, 07-03-2022 18:19 WIB]*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/*Kamus*/
+    int sisi; //sisi bujursangkar
+    int Luas; //luas bujursangkar
+
+/*Algoritma*/
+
+    printf("Masukkan sisi:");
+    scanf("%d", &sisi);
+
+    printf("%d", sisi*sisi);
+
+    return 0;
+}
